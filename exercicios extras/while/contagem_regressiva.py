68584c2c6414ca23184c975469f08d64149e0005
VivianTeixeira <vviviteixeira@hotmail.com>
2024-02-27 22:22:23 -0300

    Update contagem_regressiva.py

--- exercicios extras/while/contagem_regressiva.py
+++ exercicios extras/while/contagem_regressiva.py
@@ -2,3 +2,11 @@
 Peça ao usuário para inserir um número inteiro positivo e faça uma contagem regressiva a partir desse número até 0, imprimindo cada número no console.
 '''
 print('CONTAGEM REGRESSIVA\n')
+
+num = int(input('Digite um número: '))
+
+while num >= 0:
+    print(num)
+    num -= 1
+
+print('Fim')
