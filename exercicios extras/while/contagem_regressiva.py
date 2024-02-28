487041a45932f63708a653528a618dc3723c4395
VivianTeixeira <vviviteixeira@hotmail.com>
2024-02-27 22:18:29 -0300

    exercicio contagem regressiva
    Um exercicio o qual pede ao usuário para inserir um número inteiro positivo e faça uma contagem regressiva a partir desse número até 0, imprimindo cada número no console.

---
+++ exercicios extras/while/contagem_regressiva.py
@@ -0,0 +1,4 @@
+'''
+Peça ao usuário para inserir um número inteiro positivo e faça uma contagem regressiva a partir desse número até 0, imprimindo cada número no console.
+'''
+print('CONTAGEM REGRESSIVA\n')
